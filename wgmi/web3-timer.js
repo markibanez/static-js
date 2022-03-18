d93a542f5667e6bc548a721a32e9ed12e4caa2f6
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-18 19:27:39 +0800

    Update web3-timer.js

--- wgmi/web3-timer.js
+++ wgmi/web3-timer.js
@@ -1,5 +1,16 @@
+const connectUrl = '/connect-web3'
+
 checkNFTs = async () => {
     console.log(ethereum);
+    if (ethereum) {
+        if (ethereum.selectedAddress) {
+            console.log(ethereum.selectedAddress);
+        } else {
+            window.location.href = connectUrl;
+        }
+    } else {
+        window.location.href = connectUrl;
+    }
 }
 
 setInterval(async () => {
