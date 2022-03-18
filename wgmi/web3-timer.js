4b862656c5a86fb59d24dc950b21830d163103ce
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-18 21:05:50 +0800

    Update web3-timer.js

--- wgmi/web3-timer.js
+++ wgmi/web3-timer.js
@@ -3,13 +3,13 @@ const connectUrl = '/connect-web3'
 checkNFTs = async () => {
     if (ethereum) {
         if (ethereum.selectedAddress) {
-            console.log(ethereum.selectedAddress);
             var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
             var abi = ["function balanceOf(address owner) view returns (uint balance)"];
 
             var contract = new ethers.Contract("0xd89B00736C50C867133EBc5BF731FDbA6b29b3b7", abi, provider);
 
             contract.balanceOf(ethereum.selectedAddress).then((res) => {
+                console.log(res);
                 if (res === 0) {
                     window.location.href = `${connectUrl}?code=zero-balance`;
                 }
