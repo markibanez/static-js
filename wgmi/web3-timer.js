4d07e9c8fbf499534c4f708d50bd7912912bec2f
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-19 22:14:48 +0800

    Update web3-timer.js

--- wgmi/web3-timer.js
+++ wgmi/web3-timer.js
@@ -3,21 +3,22 @@ const connectUrl = '/connect-web3'
 checkNFTs = async () => {
     try {
         if (typeof ethereum !== 'undefined') {
-            console.log('ethereum.selectedAddress: ', ethereum.selectedAddress);
-            // if (ethereum.selectedAddress) {
-            //     var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
-            //     var abi = ["function balanceOf(address owner) view returns (uint balance)"];
+            if (ethereum.selectedAddress) {
+                console.log('ethereum.address: ', ethereum.selectedAddress);
+                var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
+                var abi = ["function balanceOf(address owner) view returns (uint balance)"];
 
-            //     var contract = new ethers.Contract("0xd89B00736C50C867133EBc5BF731FDbA6b29b3b7", abi, provider);
+                var contract = new ethers.Contract("0xd89B00736C50C867133EBc5BF731FDbA6b29b3b7", abi, provider);
 
-            //     contract.balanceOf(ethereum.selectedAddress).then((res) => {
-            //         if (res.eq(0)) {
-            //             window.location.href = `${connectUrl}?code=zero-balance`;
-            //         }
-            //     })
-            // } else {
-            //     window.location.href = `${connectUrl}?code=no-wallet`;
-            // }
+                contract.balanceOf(ethereum.selectedAddress).then((res) => {
+                    if (res.eq(0)) {
+                        window.location.href = `${connectUrl}?code=zero-balance`;
+                    }
+                })
+            } else {
+                console.log('ethereum.address: ', ethereum.selectedAddress);
+                // window.location.href = `${connectUrl}?code=no-wallet`;
+            }
         } else {
             window.location.href = `${connectUrl}?code=no-ethereum`;
         }
