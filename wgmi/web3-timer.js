a983d7ed1eb1e36d46a9db3d3ec411a7c61abf11
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-19 21:49:08 +0800

    Update web3-timer.js

--- wgmi/web3-timer.js
+++ wgmi/web3-timer.js
@@ -3,20 +3,21 @@ const connectUrl = '/connect-web3'
 checkNFTs = async () => {
     try {
         if (typeof ethereum !== 'undefined') {
-            if (ethereum.selectedAddress) {
-                var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
-                var abi = ["function balanceOf(address owner) view returns (uint balance)"];
+            console.log('ethereum.selectedAddress: ', ethereum.selectedAddress);
+            // if (ethereum.selectedAddress) {
+            //     var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
+            //     var abi = ["function balanceOf(address owner) view returns (uint balance)"];
 
-                var contract = new ethers.Contract("0xd89B00736C50C867133EBc5BF731FDbA6b29b3b7", abi, provider);
+            //     var contract = new ethers.Contract("0xd89B00736C50C867133EBc5BF731FDbA6b29b3b7", abi, provider);
 
-                contract.balanceOf(ethereum.selectedAddress).then((res) => {
-                    if (res.eq(0)) {
-                        window.location.href = `${connectUrl}?code=zero-balance`;
-                    }
-                })
-            } else {
-                window.location.href = `${connectUrl}?code=no-wallet`;
-            }
+            //     contract.balanceOf(ethereum.selectedAddress).then((res) => {
+            //         if (res.eq(0)) {
+            //             window.location.href = `${connectUrl}?code=zero-balance`;
+            //         }
+            //     })
+            // } else {
+            //     window.location.href = `${connectUrl}?code=no-wallet`;
+            // }
         } else {
             window.location.href = `${connectUrl}?code=no-ethereum`;
         }
