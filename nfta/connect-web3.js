3a35a34f1c718c65b24644a2e3e5a532d68bbe58
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-23 13:04:17 +0800

    Copy and customize code for NFTA

---
+++ nfta/connect-web3.js
@@ -0,0 +1,23 @@
+const buttonId = 'connect';
+var bt = document.getElementById(buttonId);
+
+async function hasPass() {
+    var provider = await new ethers.providers.Web3Provider(window.ethereum, 'mainnet');
+    await provider.send('eth_requestAccounts', []);
+    var signer = provider.getSigner();
+    var address = await signer.getAddress();
+    var abi = [{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+
+    var contract = new ethers.Contract('0xA3106416fDE395bf6A62B8e932dF01F5f660A5F2', abi, signer);
+
+    contract.balanceOf(address, "1").then((res) => {
+        console.log(res)
+        if (res > 0) {
+            alert("This wallet doesn't have a NFT Academy Pass, you can't access the content");
+        } else {
+            window.location.href = `/course-trainings`;
+        }
+    });
+}
+
+bt.onclick = hasPass;
---
+++ nfta/web3-timer.js
@@ -0,0 +1,37 @@
+const connectUrl = '/connect-web3'
+
+checkNFTs = async () => {
+    try {
+        if (typeof ethereum !== 'undefined') {
+            if (ethereum.selectedAddress) {
+                // console.log('ethereum.address: ', ethereum.selectedAddress);
+                var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
+                var abi =  [{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+
+                var contract = new ethers.Contract("0xA3106416fDE395bf6A62B8e932dF01F5f660A5F2", abi, provider);
+
+                contract.balanceOf(ethereum.selectedAddress, "1").then((res) => {
+                    if (res.eq(0)) {
+                        window.location.href = `${connectUrl}?code=zero-balance`;
+                    }
+                })
+            } else {
+                // console.log('ethereum.address: ', ethereum.selectedAddress);
+                window.location.href = `${connectUrl}?code=no-wallet`;
+            }
+        } else {
+            window.location.href = `${connectUrl}?code=no-ethereum`;
+        }
+    } catch (err) {
+        console.log(err);
+        window.location.href = `${connectUrl}?code=unhandled-exception`;
+    }
+}
+
+setInterval(async () => {
+    await checkNFTs();
+}, 5000);
+
+setTimeout(async () => {
+    await checkNFTs();
+}, 500);
