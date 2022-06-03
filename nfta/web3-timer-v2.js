a3bf209d68190d62bfc2c586dba59c6a1d4a385c
Mark Ibanez <mark.ibanez@gmail.com>
2022-06-03 09:07:26 +0800

    Migrated to web3Modal

--- nfta/web3-timer-v2.js
+++ nfta/web3-timer-v2.js
@@ -19,10 +19,13 @@ const web3Modal = new Web3Modal({
 
 checkNFTs = async () => {
     try {
-        if (typeof ethereum !== 'undefined') {
-            if (ethereum.selectedAddress) {
+        const web3ModalProvider = await web3Modal.connect();
+        if (web3Modal.cachedProvider) {
+            const provider = await new ethers.providers.Web3Provider(web3ModalProvider, 'mainnet');
+            const signer = await provider.getSigner();
+            const address = await signer.getAddress();
+            if (address) {
                 // console.log('ethereum.address: ', ethereum.selectedAddress);
-                var provider = await new ethers.providers.Web3Provider(window.ethereum, "mainnet")
                 var abi =  [{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"id","type":"uint256"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
 
                 var contract = new ethers.Contract("0xA3106416fDE395bf6A62B8e932dF01F5f660A5F2", abi, provider);
@@ -37,7 +40,7 @@ checkNFTs = async () => {
                 window.location.href = `${connectUrl}?code=no-wallet`;
             }
         } else {
-            window.location.href = `${connectUrl}?code=no-ethereum`;
+            window.location.href = `${connectUrl}?code=not-connected`;
         }
     } catch (err) {
         console.log(err);
